82d33aa724ed6f1dfcabf5917b1d4763c5bbb3e6
Nikita <github@nikiv.dev>
2024-12-04 13:31:21 +0100

    .

--- app/ssr.tsx
+++ app/ssr.tsx
@@ -12,6 +12,10 @@ const handler = createStartHandler({
   getRouterManifest,
 })
 
+// TODO: taken from https://github.com/TanStack/router/blob/main/examples/react/start-clerk-basic/app/ssr.tsx
+// but it's breaking on types for some reason
+// https://discord.com/channels/719702312431386674/1238170697650405547/1313838484854669362
+// https://discord.com/channels/856971667393609759/1313502145936490538/1313838763000201239
+// @ts-ignore
 const clerkHandler = createClerkHandler(handler)
-
 export default clerkHandler(defaultStreamHandler)
